4a14ab0d0920956a32a7e1753b88988894871b2d
Aleksey Belchenkov <u608110@gmail.com>
2021-07-24 13:21:46 +0400

    config & env variables

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
+require('dotenv/config');
+
 const app = express();
 
+const API_URL = process.env.API_URL || 'http://127.0.0.1';
+const API_PREFIX = process.env.API_PREFIX || '/api/v1';
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get(`${API_PREFIX}/`, (req, res) => {
    return res.send("API works...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running http://localhost:${PORT}`);
+    console.log(`Server is running ${API_URL}:${PORT}`);
 });
